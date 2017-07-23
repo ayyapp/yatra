1e889166e7d78460105020fcc95e0782e2b73023
Nivas_Automation <sahoonivas@gmail.com>
2017-07-24 00:03:37 +0530

    Play COnflict

--- Yatra/src/POM/testingGIT.java
+++ Yatra/src/POM/testingGIT.java
@@ -5,7 +5,8 @@ public class testingGIT {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Its gonna b FUN");
-
+///Conflict one
+		System.out.println("Playing with Conflicts");
 	}
 
 }
