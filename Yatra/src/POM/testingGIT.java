1c989e7865f3cad53dd19cd95f87a21b58e37645
Nivas_Automation <sahoonivas@gmail.com>
2017-07-24 00:02:55 +0530

    Maven Workspace Conflict message

--- Yatra/src/POM/testingGIT.java
+++ Yatra/src/POM/testingGIT.java
@@ -5,6 +5,8 @@ public class testingGIT {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Its gonna b FUN");
+		//Playing Conflicts
+		System.out.println("Maven Workspace Conflict message");
 
 	}
 
